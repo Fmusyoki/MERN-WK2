e341d03f6d4f8acf0ee34271afd5fb90afe852f5
Fred Musyoki <72469298+Fmusyoki@users.noreply.github.com>
2025-10-08 14:29:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -129,11 +129,8 @@ app.get('/api/products', (req, res) => {
   });
 });
 
-app.get('', (req, res) => {
-    res.send('Hello PLP');
-});
-
 app.listen(PORT, () => {
     console.log('Server is running on port${PORT}');
 })
+
 
